0068f1015754c0b0e795aa638a239b0fe274cc7f
Michał Kordulewski <michal@kordulewski.pl>
2017-09-16 22:10:31 +0200

    solution 7 for task 00

---
+++ src/main/java/pl/kordulewski/codility/task_00/Solution7.java
@@ -0,0 +1,15 @@
+package pl.kordulewski.codility.task_00;
+
+public class Solution7 implements SolutionInterface {
+    
+    private static final int CRITICAL_SIZE = 500000;
+    
+    public int solution(int[] A) {
+        if (A.length < CRITICAL_SIZE) {
+            return new Solution1().solution(A);
+        } else {
+            return new Solution6().solution(A);
+        }
+    }
+    
+}
