19c2c4dd394c5509164d7ccfaf9ea86e28dc78f5
Michał Kordulewski <michal@kordulewski.pl>
2017-09-16 22:09:29 +0200

    solution 1 for task 00

---
+++ src/main/java/pl/kordulewski/codility/task_00/Solution1.java
@@ -0,0 +1,22 @@
+package pl.kordulewski.codility.task_00;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution1 implements SolutionInterface {
+    public int solution(int[] A) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i : A) {
+            if (map.get(new Integer(i))==null) {
+                map.put(new Integer(i), 1);
+            } else {
+                map.put(new Integer(i), map.get(new Integer(i))+1);
+            }
+        }
+        int c = 1;
+        while (map.get(new Integer(c)) != null) {
+            c++;
+        }
+        return c;
+    }
+}
