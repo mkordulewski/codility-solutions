611c8f1665651d22bc66bacab12cf4657ef1bf6e
Michał Kordulewski <michal@kordulewski.pl>
2017-09-16 22:09:40 +0200

    solution 2 for task 00

---
+++ src/main/java/pl/kordulewski/codility/task_00/Solution2.java
@@ -0,0 +1,23 @@
+package pl.kordulewski.codility.task_00;
+
+import java.util.Arrays;
+
+public class Solution2 implements SolutionInterface {
+    public int solution(int[] A) {
+        Arrays.sort(A);
+        int index = 0;
+        Integer previous = null;
+        while (index<A.length) {
+            if (previous != null && previous > 0 &&(previous+1) < A[index] ) {
+                return previous+1;
+            }
+            previous = A[index];
+            index++;
+        }
+        if (previous == null || (A.length>=1 && A[A.length-1]<=0)) {
+            return 1;
+        } else {
+            return previous+1;
+        }
+    }
+}
