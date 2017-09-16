893e0b48c3fd79946a814f30353c448eb7bd293d
Michał Kordulewski <michal@kordulewski.pl>
2017-09-16 22:10:21 +0200

    solution 6 for task 00

---
+++ src/main/java/pl/kordulewski/codility/task_00/Solution6.java
@@ -0,0 +1,27 @@
+package pl.kordulewski.codility.task_00;
+
+/**
+ * Bez użycia typów zewnętrznych, bez instrukcji importu.
+ */
+public class Solution6 implements SolutionInterface {
+    public int solution(int[] A) {
+        int max = 0;
+        for (int i = 0; i<A.length;i++) {
+            if (max<A[i]) {
+                max = A[i];
+            }
+        }
+        int[] aggr = new int[max+1];
+        for (int i = 0; i<A.length;i++) {
+            if (A[i]>0) {
+                aggr[A[i]] = 1;
+            }
+        }
+        for (int i = 1; i<aggr.length;i++) {
+            if (aggr[i]==0) {
+                return i;
+            }
+        }
+        return max+1;
+    }
+}
