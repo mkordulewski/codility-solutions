2d22721e92dc990ef3a052df6e6d7275fec10215
Michał Kordulewski <michal@kordulewski.pl>
2017-09-30 19:28:00 +0200

    description for task 3.2

--- src/main/java/pl/kordulewski/codility/lesson_03_02/package-info.java
+++ src/main/java/pl/kordulewski/codility/lesson_03_02/package-info.java
@@ -1,4 +1,34 @@
 /**
- 
+ * https://codility.com/programmers/lessons/3-time_complexity/frog_jmp/
+ * <p>
+ * A small frog wants to get to the other side of the road.
+ * The frog is currently located at position X and wants to get to a position greater than or equal to Y.
+ * The small frog always jumps a fixed distance, D.
+ * <p>
+ * Count the minimal number of jumps that the small frog must perform to reach its target.
+ * <p>
+ * Write a function:
+ * <p>
+ * class Solution { public int solution(int X, int Y, int D); }
+ * that, given three integers X, Y and D, returns the minimal number of jumps from position X to a position equal to or greater than Y.
+ * <p>
+ * For example, given:
+ * <p>
+ * X = 10
+ * Y = 85
+ * D = 30
+ * the function should return 3, because the frog will be positioned as follows:
+ * <p>
+ * after the first jump, at position 10 + 30 = 40
+ * after the second jump, at position 10 + 30 + 30 = 70
+ * after the third jump, at position 10 + 30 + 30 + 30 = 100
+ * Assume that:
+ * <p>
+ * X, Y and D are integers within the range [1..1,000,000,000];
+ * X ≤ Y.
+ * Complexity:
+ * <p>
+ * expected worst-case time complexity is O(1);
+ * expected worst-case space complexity is O(1).
  */
 package pl.kordulewski.codility.lesson_03_02;
